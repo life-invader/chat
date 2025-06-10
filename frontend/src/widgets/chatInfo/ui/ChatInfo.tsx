da5153253d7198df0ad12db9316d115020689151
Alexander Leontyev <orientstar96@gmail.com>
2025-06-10 22:58:11 +0500

    feat: add user info in chat user profile

--- frontend/src/widgets/chatInfo/ui/ChatInfo.tsx
+++ frontend/src/widgets/chatInfo/ui/ChatInfo.tsx
@@ -9,6 +9,17 @@ export const ChatInfo = () => {
         <Button iconLeft={{ icon: 'close' }} isBgTransparent={true} />
         <h3 className="chatInfo__topTitle">Contact info</h3>
       </div>
+
+      <div className="chatInfo__user">
+        <div className="chatInfo__userImgContainer">
+          <img src={userProfileImg} alt="" width={124} height={124} loading="lazy" />
+        </div>
+
+        <p className="chatInfo__userName">Lara Mueller</p>
+        <a className="chatInfo__userPhoneNumber" href="tel:491522792358">
+          +49 1522 792358
+        </a>
+      </div>
     </div>
   );
 };
